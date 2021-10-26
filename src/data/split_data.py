e38a3796a138428943c0f2c56632d2ca263b5956
naresh <naresh.datascience@gmail.com>
2021-10-26 17:17:14 +0530

    Added Split data file

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -1,30 +1,32 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import argparse
+import pandas as pd
+from load_data import read_params
+from sklearn.model_selection import train_test_split
 
+def split_data(df,train_data_path,test_data_path,split_ratio,random_state):
+    train, test = train_test_split(df,test_size=split_ratio, random_state=random_state)
+    train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
+    test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")  
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def split_and_saved_data(config_path):
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    split the train dataset(data/raw) and save it in the data/processed folder
+    input: config path 
+    output: save splitted files in output folder
+    """
+    config = read_params(config_path)
+    raw_data_path = config["raw_data_config"]["raw_data_csv"]
+    test_data_path = config["processed_data_config"]["test_data_csv"] 
+    train_data_path = config["processed_data_config"]["train_data_csv"]
+    split_ratio = config["raw_data_config"]["train_test_split_ratio"]
+    random_state = config["raw_data_config"]["random_state"]
+
+    raw_df = pd.read_csv(raw_data_path)
+    split_data(raw_df,train_data_path,test_data_path,split_ratio,random_state)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    split_and_saved_data(config_path=parsed_args.config)
