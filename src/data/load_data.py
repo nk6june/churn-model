60b9f0c440e444469f8d744bc771fc212f133e21
naresh <naresh.datascience@gmail.com>
2021-10-26 16:08:28 +0530

    Added requirements & other files

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,2 +1,12 @@
 import yaml
-import argparser
+import argparse
+import numpy as np
+import pandas as pd
+
+def read_params(config_path):
+    """
+    Read parameters from the params.yaml file
+    input: params.yaml location
+    output: parameters as dictionary
+    """
+    pass
