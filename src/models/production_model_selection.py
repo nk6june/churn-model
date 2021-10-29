73b9f628f046cf503753b198bc78536627ff61de
naresh <naresh.datascience@gmail.com>
2021-10-29 23:25:44 +0530

    Added model joblib file and flask files

--- src/models/production_model_selection.py
+++ src/models/production_model_selection.py
@@ -2,12 +2,12 @@ import joblib
 import mlflow
 import argparse
 from pprint import pprint
-from mlflow.tracking import MlflowClient
 from src.models.train_model import read_params
+from mlflow.tracking import MlflowClient
 
 def log_production_model(config_path):
     config = read_params(config_path)
-    mlflow_config = config["mlflow_config"]
+    mlflow_config = config["mlflow_config"] 
     model_name = mlflow_config["registered_model_name"]
     model_dir = config["model_dir"]
     remote_server_uri = mlflow_config["remote_server_uri"]
@@ -16,12 +16,12 @@ def log_production_model(config_path):
     runs = mlflow.search_runs(experiment_ids=1)
     max_accuracy = max(runs["metrics.accuracy"])
     max_accuracy_run_id = list(runs[runs["metrics.accuracy"] == max_accuracy]["run_id"])[0]
-
+    
     client = MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
         mv = dict(mv)
 
-        if mv["run_id"]==max_accuracy_run_id:
+        if mv["run_id"] == max_accuracy_run_id:
             current_version = mv["version"]
             logged_model = mv["source"]
             pprint(mv, indent=4)
@@ -46,8 +46,3 @@ if __name__ == '__main__':
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     data = log_production_model(config_path=parsed_args.config)
-
-
-
-
-
